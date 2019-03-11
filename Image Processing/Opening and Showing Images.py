2eac8184e66b207766a827e4604ad83fb56944b3
Matthew Wardle <mattydwardle@gmail.com>
2019-03-11 19:48:44 +0000

    Removing old file names

--- Image Processing/Opening and Showing Images.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-import cv2
-img = cv2.imread("breakers.jpg", 0)
-cv2.imshow("image",img)
--- Image Processing/Video based maipulation/Combining the Stolen and Frame Extraction.py
+++
@@ -1,7 +0,0 @@
-import cv2
-import argparse
-import os
-ap = argparse.ArgumentParser()
-ap.add_argument("-ext","--extension", required=False,default='png',help="extention name. default is 'png'.")
-ap.add_argument("-o","--output",required=False,default="output.mp4",help="output vidoe file")
-args=vars(ap.parse_args())
