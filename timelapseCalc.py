1a9e1677fcd51396622f4cd8cffb500596927685
Crazy-Ginger <mattydwardle@gmail.com>
2019-06-09 01:42:29 +0100

    Added options to timelapseCalc

--- timelapseCalc.py
+++ timelapseCalc.py
@@ -1,28 +1,36 @@
 from math import floor as floor
-start = str(input("enter start time: "))
-stop = str(input("enter end time: "))
-
-sHour = int(start[:2])
-sMin = int(start[2:4])
-
-eHour = int(stop[:2])
-eMin = int(stop[2:4])
-
-time = 0
-
-if eHour < sHour:
-    time = time + eHour*60 + (24-sHour-1)*60
-    #print ((24-sHour)*60)
-    #print (eHour*60)
-    #print (time)
-    
+import datetime
+def time(nowOrNot = True):
+    if nowOrNot:
+        currentDT = datetime.datetime.now()
+        sHour = int(currentDT.hour)
+        sMin = int(currentDT.minute)
+    else:
+        start = str(input("enter start time: "))
+        sHour = int(start[:2])
+        sMin = int(start[2:4])
+    ######################
+    stop = str(input("enter end time: "))
+    eHour = int(stop[:2])
+    eMin = int(stop[2:4])
+    time = 0
+    #######################
+    if eHour < sHour:
+        time = time + eHour*60 + (24 - sHour - 1)*60
+    else:
+        #print("running else")
+        time = (eHour - sHour)*60
+    #######################print(time)
+    if sMin != 0:
+        time = time + eMin + (60-sMin)
+    else:
+       time = time + eMin 
+    #######################
+    print ("time = ",time)
+    print (floor(time/60), " hours and ", time%60, " minutes")
+#____________________________________________________________#
+choice = str(input("Is the start time now? "))
+if choice=="yes" or choice=="Yes" or choice=="y" or choice=="Y" or choice=="YES":
+    time(True)
 else:
-    #print("running else")
-    time = (eHour - sHour)*60
-
-    
-#print(time)
-time = time + eMin + (60-sMin)
-
-print ("time = ",time)
-print (floor(time/60), " hours and ", time%60, " minutes")
+    time(False)
