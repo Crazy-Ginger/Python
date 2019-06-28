841572ae9c36aea811c820252748f49f6b419771
Matthew Wardle <mattydwardle@gmail.com>
2019-06-28 03:12:00 +0000

    Worked on prob80 but to no avail, asked Laura for help

--- Problem80.py
+++ Problem80.py
@@ -1,23 +1,18 @@
+#!/usr/bin/env python3
 from decimal import *
 total = 0
 #sets the number of decimal places for floats to 100
-getcontext().prec = 100
+getcontext().prec = 101
 
 for i in range(1, 100):
     numb = str(Decimal(i).sqrt())
-    cont = False
-    
-    for char in numb:
-        if char == '.':
-            cont = True
-            break
-    #print(i)   
-        #print (numb)
-    if cont == True:
-        for char in reversed(numb):
+    temp = 0
+    numb = numb[:-1]
+    if len(numb) != 1:
+        for char in numb:
             if char != '.':
+                temp += int(char)
                 total += int(char)
-            else:
-                break
-    print ("i: ", i , "\ttotal: ", total)       
-print(total)
+        print(i, "\tadding: ", temp)
+        # print("total: ", total, "\n")
+print("Total: ", total)
