0b6685ff6660270f7b43ea7e4eb010a8f00d39f9
Matthew Wardle <mattydwardle@gmail.com>
2019-07-01 23:39:47 +0000

    Working on 26

--- Problem26.py
+++ Problem26.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 from decimal import *
-getcontext().prec = 20
-
+getcontext().prec = 100
+longer = 0
 for i in range(1, 1000):
     temp = str((Decimal(1)/Decimal(i)))
     print (temp, "\t", len(temp))
+    if (len(temp)>longer):
+        longer = len(temp)
+
+print("longest: ", longer)
 
