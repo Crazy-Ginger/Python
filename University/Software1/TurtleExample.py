d0af5936dec56655cc8a6761299b266435697175
Rebecca <mattydwardle@gmail.com>
2019-10-16 11:05:43 +0100

    Added a couple of uni files, not on system currently

---
+++ University/Software1/TurtleExample.py
@@ -0,0 +1,14 @@
+import turtle
+from math import cos
+
+window = turtle.Screen()
+
+window = turtle.Screen()
+alex = turtle.Turtle()
+alex.speed(10)
+alex.pensize(1)
+step = 5
+
+for i in range (10,200000,2):
+    alex.forward(50)
+    alex.right(i)
