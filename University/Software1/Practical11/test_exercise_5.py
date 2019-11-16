0e7646abd5a864bf6a8ea23c845848fb705011f9
Rebecca Wardle <mattydwardle@gmail.com>
2019-11-16 17:06:39 +0000

    Added gitignore to avoid __pycache__

--- University/Software1/Practical11/test_exercise_5.py
+++ University/Software1/Practical11/test_exercise_5.py
@@ -9,16 +9,16 @@ import inspect
 from recursion import flatten
 
 class TestExercise5(unittest.TestCase):
-    
+
     def test_flatten_flatlist(self):
         self.assertEqual([0,1,2,3,4], flatten([0,1,2,3,4]), "flatten([0,1,2,3,4]) incorrect")
 
     def test_flatten_empty(self):
         self.assertEqual([], flatten([]), "flatten([]) incorrect!")
-        
+
     def test_flatten_nestedlist(self):
         self.assertEqual([1,1,2,3,3,4], flatten([[1], [1,[2,3]],3,4]), "flatten([[1], [1,[2,3]],3,4]) incorrect!")
-    
+
     def test_flatten_mixedemptylist(self):
         self.assertEqual([1,2,3], flatten([[1], [], [], [2,[],[3]]]), "flatten([[1], [], [], [2,[],[3]]]) incorrect!")
 
