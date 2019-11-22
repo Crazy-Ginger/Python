f21b1c9528205ad2faf5427de18e3aa7f1636122
Matthew Wardle <mattydwardle@gmail.com>
2019-11-22 12:21:14 +0000

    Still not sure how to do Prac12

--- University/Software1/Practical12/practical12.py
+++ University/Software1/Practical12/practical12.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
 
+def __upside_down(char):
+    if char != "?":
+        return char
+    else:
+        return "1"
+
 def wildcard_pattern(card):
+    string = []
     if '?' not in card:
         return card
     else:
-        if card[0] == "?":
+        for i in range(len(card)):
+            if card[i] == "?":
+                
+
+            
             
 
 wildcard_pattern("word")
