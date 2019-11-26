12e40badc866d0ebfa518b3dcc0fe8a38e797afb
Rebecca Wardle <mattydwardle@gmail.com>
2019-11-26 00:54:23 +0000

    Working on prac12

--- University/Software1/Practical12/practical12.py
+++ University/Software1/Practical12/practical12.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env python3
 
-def __upside_down(char):
-    if char != "?":
-        return char
-    else:
-        return "1"
-
 def wildcard_pattern(card):
-    string = []
     if '?' not in card:
         return card
     else:
-        for i in range(len(card)):
-            if card[i] == "?":
-                
+        if card[0] == "?":
+            print ("token statement, replace")
 
-            
-            
+
+def allwords(digits, keypad):
+    digits = str(digits)
+
+    comb = {}
+    for char in digits:
+
 
 wildcard_pattern("word")
 wildcard_pattern("wor?")
