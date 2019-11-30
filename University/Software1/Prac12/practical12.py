56f9482fc4d1654f077b167a1dd13eb9ac250b59
Rebecca Wardle <mattydwardle@gmail.com>
2019-11-30 03:09:08 +0000

    Still failing at question 1

--- University/Software1/Prac12/practical12.py
+++ University/Software1/Prac12/practical12.py
@@ -1,14 +1,39 @@
 #!/usr/bin/env python3
 
-def wildcard_pattern(card, l= 0):
-    print (l, card)
-    if "?" not in card:
-        return [card, card]
-    elif card[0] == "?":
-        return ["0"+ wildcard_pattern(card[1:], l+1)[0], "1" + wildcard_pattern(card[1:], l+1)[1]]
+def wildcard_pattern(card, pat):
+    words = []
+    if len(card) == 0:
+        return ''
+    elif pat not in card:
+        return card
+    for i in range(len(card)):
+        if card[i] == pat and len(words) == 0:
+            words.append("0")
+            words.append("1")
+        elif card[i] == pat:
+            words = __caller(words)
 
+        else:
+            for j in range(len(words)):
+                words[j] += card[i]
+        # print (i, words)
+    words = sorted(words)
+    # print ("\n\n")
+    return words
 
-#wildcard_pattern("word")
-print (wildcard_pattern("?01010"))
+def __caller(words):
+    n_words = []
+    alter = True
+    for i in range(len(words)*2):
+        if alter:
+            alter = False
+            n_words.append(words[i//2] + "1")
+        else:
+            alter = True
+            n_words.append(words[i//2] + "0")
 
-print (wildcard_pattern("??1010"))
+    return n_words
+
+# print (wildcard_pattern("?01010"))
+# print (wildcard_pattern("??0101"))
+# print (wildcard_pattern("???"))
