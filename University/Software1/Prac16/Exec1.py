873fb58fd7c6c295445bdcefaae44a1ea02b9fac
Matthew Wardle <mattydwardle@gmail.com>
2019-12-04 11:32:17 +0000

    Jumped to prac16 to keep up

---
+++ University/Software1/Prac16/Exec1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+class ListNode():
+    def __init__(self, data=None):
+        self.data = data
+        self._next = ListNode()
+
+        
+class LinkedStack():
+    def __init__(self):
+        self._top = None
+        
+    def __str__(self);
+        
