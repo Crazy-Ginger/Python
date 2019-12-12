75b60dc005416d768f4c2659dd27415f46c78b37
Rebecca Wardle <mattydwardle@gmail.com>
2019-12-12 00:41:05 +0000

    Solved day 2, again after reading the whole question

--- University/AdventCode1/day2.py
+++ University/AdventCode1/day2.py
@@ -3,23 +3,38 @@
 def intcoder(intcode):
     for i in range(0, len(intcode), 4):
         if intcode[i] == 99:
-            print (i, "\t", intcode[i])
+            # print (i, "\t", intcode[i])
             break
 
         if intcode[i] == 1:
-            intcode[intcode[i+3]] = intcode[intcode[i+1]] + intcode[intcode[i+2]]
+            try:
+                intcode[intcode[i+3]] = intcode[intcode[i+1]] + intcode[intcode[i+2]]
+            except:
+                return
 
         elif intcode[i] == 2:
-            intcode[intcode[i+3]] = intcode[intcode[i+1]] * intcode[intcode[i+2]]
+            try:
+                intcode[intcode[i+3]] = intcode[intcode[i+1]] * intcode[intcode[i+2]]
+            except:
+                return
 
         else:
-            print ("Error:",intcode[i])
-    return intcode
+            print ("Error:",i)
+    return intcode[0]
 
-intcode = [1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,19,1,5,19,23,2,6,23,27,1,27,5,31,2,9,31,35,1,5,35,39,2,6,39,43,2,6,43,47,1,5,47,51,2,9,51,55,1,5,55,59,1,10,59,63,1,63,6,67,1,9,67,71,1,71,6,75,1,75,13,79,2,79,13,83,2,9,83,87,1,87,5,91,1,9,91,95,2,10,95,99,1,5,99,103,1,103,9,107,1,13,107,111,2,111,10,115,1,115,5,119,2,13,119,123,1,9,123,127,1,5,127,131,2,131,6,135,1,135,5,139,1,139,6,143,1,143,6,147,1,2,147,151,1,151,5,0,99,2,14,0,0]
+orig_intcode = [1,30,50,3,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,19,1,5,19,23,2,6,23,27,1,27,5,31,2,9,31,35,1,5,35,39,2,6,39,43,2,6,43,47,1,5,47,51,2,9,51,55,1,5,55,59,1,10,59,63,1,63,6,67,1,9,67,71,1,71,6,75,1,75,13,79,2,79,13,83,2,9,83,87,1,87,5,91,1,9,91,95,2,10,95,99,1,5,99,103,1,103,9,107,1,13,107,111,2,111,10,115,1,115,5,119,2,13,119,123,1,9,123,127,1,5,127,131,2,131,6,135,1,135,5,139,1,139,6,143,1,143,6,147,1,2,147,151,1,151,5,0,99,2,14,0,0]
 
-intcoder(intcode)
 
-print (intcode,"\n")
-print (len(intcode), "\t", len(intcode)/4)
-print ("intcode[0]:",intcode[0])
+intcode = orig_intcode.copy()
+
+for i in range(100):
+    for j in range(100):
+        intcode = orig_intcode.copy()
+        print ("i:",i,"j:",j)
+        intcode[1] = i
+        intcode[2] = j
+
+        if intcoder(intcode) == 19690720:
+            print ("i:",i,"\tj:",j)
+            input()
+        print ("intcode[0]:", intcode[0])
