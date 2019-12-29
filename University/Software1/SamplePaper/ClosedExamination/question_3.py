3ec99652ab7b8e66e825f7d39d08414e39aa29d9
Rebecca <mattydwardle@gmail.com>
2019-12-29 12:17:55 +0000

    Working on question 3 of practice

--- University/Software1/SamplePaper/ClosedExamination/question_3.py
+++ University/Software1/SamplePaper/ClosedExamination/question_3.py
@@ -2,4 +2,14 @@
 def two_out_five(message):
     if not isinstance(message, str):
         raise TypeError("expected str")
-    
+    if len(message) % 5 != 0:
+        raise ValueError("Message incorrect length")
+
+    adders = [7, 4, 2, 1, 0]
+    for i in range(0, len(message), 5):
+        one_count = 0
+        total = 0
+        for j in range(0, 5):
+            if message[i+j] == 1:
+                one_count += 1
+                total += adders
