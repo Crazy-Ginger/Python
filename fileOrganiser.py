6512c0ae59ee4eaa8002c03f6f31110fc89175a0
Rebecca <mattydwardle@gmail.com>
2020-03-30 03:04:04 +0100

    File organised may now work but still relies on system file creation dates

--- fileOrganiser.py
+++ fileOrganiser.py
@@ -30,12 +30,12 @@ else:
 os.chdir(origin_path)
 
 if args.destination:
-    destination_path = args.destination
+    dest_path = args.destination
 else:
-    destination_path = "../Photos"
+    dest_path = "../Photos"
 
-if os.path.isdir(destination_path) == False:
-    os.mkdir(destination_path)
+if os.path.isdir(dest_path) == False:
+    os.mkdir(dest_path)
     # used to delete the pre-existing file structure with the sorted photos(?), now it should just add ones that don't alread exist
     # rmtree(dir_Name, True)
 
@@ -46,28 +46,43 @@ months = ["01-Jan", "02-Feb", "03-Mar", "04-Apr", "05-May", "06-Jun", "07-Jul",
 strange_log = ""
 # iterates over all files in the directory collecting datestamps and placing them into directories of those dates
 for file_Name in os.listdir():
-    if not fName.endswith(".JPG") or not fName.endswith(".CR2") or not fName.endswith(".MOV"):
+    if not file_Name.endswith(".JPG") or not file_Name.endswith(".CR2") or not file_Name.endswith(".MOV"):
         continue
-    file_Year = datetime.fromtimestamp(os.path.getmtime(fName)).strftime('%Y')
-    file_Month = months[int(datetime.fromtimestamp(os.path.getmtime(fName)).strftime('%m'))-1]
-    file_Date = datetime.fromtimestamp(os.path.getmtime(fName)).strftime('%d')
+    file_Year = datetime.fromtimestamp(os.path.getmtime(file_Name)).strftime('%Y')
+    file_Month = months[int(datetime.fromtimestamp(os.path.getmtime(file_Name)).strftime('%m'))-1]
+    file_Date = datetime.fromtimestamp(os.path.getmtime(file_Name)).strftime('%d')
 
     # checks if the file under that name exists, if it is not the same moves the file with an appendix, otherwise skips
     appendix = ""
-    if os.path.isfile(destination) == True:
-        if os.path.getsize(origin) == os.path.getsize(destination):
-            if cmp(fName, destination) == True:
-                #add code
+    toCopy = True
+    while (os.path.isfile(dest_path + "/" + file_Name + appendix) == True):
+        if os.path.getsize(file_Name) == os.path.getsize(dest_path + "/" + file_Name + appendix):
+            if cmp(file_Name, dest_path + "/" + file_Name + "appendix") == True:
+                toCopy = False
+                break
+
+        if appendix == "":
+            appendix = "_1"
+        elif appendix[1] == "9":
+            raise IOError("To many duplicates of:", origin_path + "/" + file_Name)
+        else:
+            strange_log += "File: " + str(origin_path) + "/" + str(file_Name) + "had same name as " + str(dest_path) +"/"+str(file_Name) +"\n"            
+            appendix = "_" + str(int(appendix[1])+1)
+
+    if toCopy == False:
+        strange_log += "File: " + str(origin_path) + "/" + str(file_Name) + "was a duplicate\n"
+        continue
 
     # creates the year, month & date directories if they don't already exist
-    if os.path.isdir(dir_Name +"/"+file_Year) == False:
-        os.mkdir(dir_Name +"/"+file_Year)
-    if os.path.isdir(dir_Name +"/"+file_Year+"/"+file_Month) == False:
-        os.mkdir(dir_Name +"/"+file_Year+"/"+file_Month)
-    if os.path.isdir(dir_Name +"/"+file_Year+"/"+file_Month+"/"+file_Date) == False:
-        os.mkdir(dir_Name +"/"+file_Year+"/"+file_Month+"/"+file_Date)
+    if os.path.isdir(dest_path + "/" + file_Year) == False:
+        os.mkdir(dest_path +"/" + file_Year)
+    if os.path.isdir(dest_path + "/" + file_Year + "/" + file_Month) == False:
+        os.mkdir(dest_path + "/" + file_Year + "/" + file_Month)
+    if os.path.isdir(dest_path + "/" + file_Year + "/" + file_Month + "/" + file_Date) == False:
+        os.mkdir(dest_path + "/" + file_Year + "/" + file_Month + "/" + file_Date)
 
     if args.copy:
-        copy2(file_Name, dest_File + file_Name + appendix)
+        copy2(file_Name, dest_path + "/" + file_Year + "/" + file_Month + "/" + file_Date + "/" + file_Name + appendix)
     else:
-        move(file_Name, dest_File +"/" + file_Year + "/" + file_Month + "/" + file_Date + "/" + file_Name + appendix)
+        move(file_Name, dest_path + "/" + file_Year + "/" + file_Month + "/" + file_Date + "/" + file_Name + appendix)
+
